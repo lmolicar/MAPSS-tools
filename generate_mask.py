d00d5783f15f1f6dbc171f18817154b9ebc038fb
Luis Molina <luis.molinacarpio@gmail.com>
2026-04-03 10:00:48 -0300

    Now the working directory can be specified via a command line parameter (work_dir).

--- generate_mask.py
+++ generate_mask.py
@@ -1,6 +1,20 @@
+import argparse
 import numpy as np
+import os
 import rioxarray
 import xarray as xr
+
+parser = argparse.ArgumentParser(prog = "generate_mask.py",
+                                 description = "Generate a mask based on the input files for a MAPSS simulation")
+
+parser.add_argument("--work_dir",
+                    help = "Where input files are located. The output will be stored in the same location.",
+                    required = False,
+                    default = "")
+
+args = parser.parse_args()
+work_dir = args.work_dir
+
 
 data_dict = {
     "elev" : 1,
@@ -16,8 +30,9 @@ data_dict = {
 def squeeze_grid(varname):
 
     src_file_name = f"{varname}.tif"
+    src_path = os.path.join(work_dir, src_file_name)
     
-    ds = rioxarray.open_rasterio(src_file_name)
+    ds = rioxarray.open_rasterio(src_path)
 
     arr = ds.values.squeeze()
 
@@ -47,7 +62,8 @@ for var in data_dict.keys():
 total = np.sum(np.stack(grids), axis = 0) * 0 + 1
 
 # Let's take the coordinates from the elevation file
-ds = rioxarray.open_rasterio("elev.tif")
+path_elev_file = os.path.join(work_dir, "elev.tif")
+ds = rioxarray.open_rasterio(path_elev_file)
 
 out_da = xr.DataArray(total,
                       dims = ["y", "x"],
@@ -59,4 +75,6 @@ out_da = xr.DataArray(total,
 out_da = out_da.rio.write_crs("epsg:4326")
 out_da = out_da.rio.write_nodata(-9999.)
 
-out_da.rio.to_raster("mask.tif", driver = "GTiff", compress = "LZW")
+dst_path = os.path.join(work_dir, "mask.tif")
+
+out_da.rio.to_raster(dst_path, driver = "GTiff", compress = "LZW")
